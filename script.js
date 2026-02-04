b329abcdea6c3763897771d461b69eb18a6340d6
Ram-ziggandzagg <ram.wadher@ziggandzagg.com>
2026-02-04 23:04:39 +0000

    enter button changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const enterBtn = document.getElementById("enterBtn");
 const intro = document.getElementById("intro");
 const site = document.getElementById("site");
+document.getElementById("enterBtn").onclick = () => alert("clicked");
 
 enterBtn.addEventListener("click", () => {
   intro.classList.add("open");
